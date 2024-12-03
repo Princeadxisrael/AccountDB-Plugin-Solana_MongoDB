018f9bb110d7bc1ff0faaa5d91cf59f8302bea96
aseneca <princeosuh@gmail.com>
2024-12-03 19:27:33 +0100

    define TokenSecondaryIndexEntry for indexing token accounts in the plugin

--- custom_geyser_plugin/src/mongodb_client.rs
+++ custom_geyser_plugin/src/mongodb_client.rs
@@ -24,6 +24,21 @@ const ACCOUNT_COLUMN_COUNT: usize = 10;
 const DEFAULT_PANIC_ON_DB_ERROR: bool = false;
 const DEFAULT_STORE_ACCOUNT_HISTORICAL_DATA: bool = false;
 
+const TOKEN_INDEX_COLUMN_COUNT: usize = 3;
+/// Struct for the secondary index for both token account's owner and mint index,
+pub struct TokenSecondaryIndexEntry {
+    /// In case of token owner, the secondary key is the Pubkey of the owner and in case of
+    /// token index the secondary_key is the Pubkey of mint.
+    secondary_key: Vec<u8>,
+
+    /// The Pubkey of the account
+    account_key: Vec<u8>,
+
+    /// Record the slot at which the index entry is created.
+    slot: i64,
+}
+
+
 struct MongodbClientWrapper {
     client: mongodb::Client,
     accounts_collection:mongodb::Collection<DbAccountInfo>,
